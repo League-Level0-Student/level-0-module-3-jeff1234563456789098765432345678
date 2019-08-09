7d64e144321092163bdb74367cac00ed0da7fe4b
jeff1234563456789098765432345678 <36941586+jeff1234563456789098765432345678@users.noreply.github.com>
2019-08-08 18:28:56 -0700

    Years_alive :smile:

---
+++ src/_07_years_alive/Years_alive.java
@@ -0,0 +1,18 @@
+package _07_years_alive;
+
+import javax.swing.JOptionPane;
+
+public class Years_alive {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 13; i++) {
+			
+	
+	JOptionPane.showMessageDialog(null,"in "+(i+2007)+" I was "+ i +" years old.");
+		
+			
+			System.out.println(i+2007);
+		}
+	}
+
+}
