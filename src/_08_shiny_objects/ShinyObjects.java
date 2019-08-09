6e23d5671d67acd23d2f7fed1c7b2359278a1df5
jeff1234563456789098765432345678 <36941586+jeff1234563456789098765432345678@users.noreply.github.com>
2019-08-08 18:29:37 -0700

    ShinyObjects :Smile:

--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -4,20 +4,30 @@
 package _08_shiny_objects;
 
 import java.applet.AudioClip;
+import java.lang.invoke.CallSite;
+import java.sql.CallableStatement;
+import java.util.concurrent.Callable;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+		String answer = JOptionPane.showInputDialog("How many shiny objects do you want?");
 		// 3. Play the sound that many times
-
+		
+			for (int i = 0; i < Integer.parseInt(answer); i++) {
+			System.out.println(i);
+			ShinyObjects so = new ShinyObjects();
+			so.playMisterZee();
+			}
 		// 1. Call the method below
-
+		 
+		
 	}
 
 	public static void playMisterZee() {
